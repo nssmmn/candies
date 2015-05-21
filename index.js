d90d99d27e916171d3c4511fcc7a16ab46e49b67
nssmmn <nssmmn@gmail.com>
2015-05-21 12:58:19 +0100

    Added normal distribution

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ _uniform.continous = _uniform_c;
 _uniform.discrete = _uniform_d;
 
 function _poisson (n,lambda){
+  // Donald Knuth algorithm
   var sample = new Array(n);
   var l = Math.exp(-lambda);
   for (var i = 0 ; i < sample.length ; i++){
@@ -32,7 +33,26 @@ function _poisson (n,lambda){
   return sample;
 }
 
+function _normal(n,m,sd){
+  //Box-Muller transform
+  if( m === undefined) m=0;
+  if( sd === undefined) sd=1;
+  var sample = [];
+  var u1,u2,z0,z1;
+  for (var i = 0 ; i < Math.round(n/2) ; i++){
+      u1 = Math.random();
+      u2 = Math.random();
+      z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2*Math.PI*u2) * sd + m;
+      sample.push(z0);
+      z1 = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(2*Math.PI*u2) * sd + m;
+      sample.push(z1);
+  }
+  if (n%2) sample.pop();
+  return sample;
+}
+
 module.exports={
   uniform : _uniform,
-  poisson : _poisson
+  poisson : _poisson,
+  normal : _normal
 };
