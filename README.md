2c2b33337e5d991c25fbafba6d190e3f0c370f41
nssmmn <nssmmn@gmail.com>
2015-05-24 09:41:03 +0100

    index.js and README.md modified

--- README.md
+++ README.md
@@ -2,24 +2,29 @@
 # candies
 A node.js package to generate a sample of length n.
 
+Until now there are 3 distributions:
+
+* Uniform distribution (discrete and continuous).
+* Poisson distribution.
+* Normal (Gaussian) distribution.
+
 ## Usage
 ```js
 var C = require('candies');
-var sample = C.distribution_name(n,param1,param2,...); // return an array of length n.
-```
-Up to now there are 3 distributions:
 
-Uniform distribution :
-```js
-var sample1 = C.uniform.continuous(n,a,b); // equivalent to C.uniform(n,a,b);
-var sample2 = C.uniform.discrete(n,a,b);
-```
-Poisson distribution :
-```js
-var sample3 = C.poisson(n,lambda);
-```
-Normal distribution :
-```js
-var sample4 = C.normal(n,m,sd);
-var sample5 = C.normal(n); // equivalent to C.normal(n,0,1)
+var n = 10; // For a sample of length n.
+
+//Uniform distribution
+var sample1 = C.uniform.continuous(n,0,2);  // equivalent to C.uniform(n,0,2);
+var sample2 = C.uniform.continuous(n,5); // equivalent to C.uniform.continuous(n,5,6);
+var sample3 = C.uniform.continuous(n); // equivalent to C.uniform.continuous(n,0,1);
+
+var sample4 = C.uniform.discrete(n,0,5);
+
+//Poisson distribution
+var sample5 = C.poisson(n,1);
+
+//Normal distribution
+var sample6 = C.normal(n,2,2); // equivalent to C.gaussian(n,2,2)
+var sample7 = C.normal(n); // equivalent to C.normal(n,0,1)
 ```
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function _uniform_c(n,a,b){
-  var sample = new Array(n);
-  for (var i = 0 ; i < sample.length ; i++){
-    sample[i] = a + (b-a) * Math.random();
+  a = a || 0;
+  b = b || a+1;
+  var sample = [];
+  for (var i = 0 ; i < n ; i++){
+    sample.push( a + (b-a) * Math.random() );
   }
   return sample;
 }
 
 function _uniform_d (n,a,b){
-  var sample = new Array(n);
-  for (var i = 0 ; i < sample.length ; i++){
-    sample[i] = Math.round(a - 0.5 + (b + 0.5 - a) * Math.random());
+  a = a || 0;
+  b = b || a+1;
+  var sample = [];
+  for (var i = 0 ; i < n ; i++){
+    sample.push( Math.round(a-0.5 + (b-a+0.5) * Math.random()));
   }
   return sample;
 }
@@ -20,31 +24,31 @@ _uniform.discrete = _uniform_d;
 
 function _poisson (n,lambda){
   // Donald Knuth algorithm
-  var sample = new Array(n);
+  var sample = [];
   var l = Math.exp(-lambda);
-  for (var i = 0 ; i < sample.length ; i++){
+  for (var i = 0 ; i < n ; i++){
     var k = 0, p= 1;
     do {
       k++;
       p *= Math.random();
     } while (p > l);
-    sample[i] = k-1 ;
+    sample.push(k-1) ;
   }
   return sample;
 }
 
 function _normal(n,m,sd){
   //Box-Muller transform
-  if( m === undefined) m=0;
-  if( sd === undefined) sd=1;
+  m = m || 0;
+  sd = sd || 1;
   var sample = [];
   var u1,u2,z0,z1;
   for (var i = 0 ; i < Math.round(n/2) ; i++){
       u1 = Math.random();
       u2 = Math.random();
       z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2*Math.PI*u2) * sd + m;
-      sample.push(z0);
       z1 = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(2*Math.PI*u2) * sd + m;
+      sample.push(z0);
       sample.push(z1);
   }
   if (n%2) sample.pop();
@@ -54,5 +58,6 @@ function _normal(n,m,sd){
 module.exports={
   uniform : _uniform,
   poisson : _poisson,
-  normal : _normal
+  normal : _normal,
+  gaussian : _normal
 };
